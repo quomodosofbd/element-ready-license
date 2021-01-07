41dd85ee31131e845eebdcf9d785b0fdff7978c8
rayhanuddin2019 <47168831+rayhanuddin2019@users.noreply.github.com>
2021-01-07 15:42:53 +0600

    added

--- src/service/License.php
+++ src/service/License.php
@@ -1,11 +1,34 @@
 <?php 
 namespace Element_Ready_Pro\License\service;
+if ( ! defined( 'ABSPATH' ) ) exit;
 
-class License
+Class License
 {
-    public function greet($greet = "Hello World")
+    private static $obj;
+    private final function __construct()
     {
-        return $greet;
+        
+      add_action( 'rest_api_init', [$this,'license_key'] );
+    }
+    public static function getInit() {
+
+        if(!isset(self::$obj)) {
+            self::$obj = new License();
+        }
+        return self::$obj;
+    }
+  
+    protected function license_key(){
+
+        register_rest_route( 'element-ready-pro/v1', '/varify', array(
+            'methods'  => \WP_REST_Server::READABLE,
+            'callback' => [$this,'generate_service'],
+        ) );
+
+    }
+
+    protected function generate_service(){
+        return rest_ensure_response( 'Hello World, this is the WordPress REST API' );
     }
 }
 
